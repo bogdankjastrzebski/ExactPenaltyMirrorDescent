a56bd9f1b53aa26ce82c575d0ac29939d71df8d5
bogdankjastrzebski <bogdan.k.jastrzebski@gmail.com>
2025-01-30 14:47:14 +0100

    julia project

---
+++ src/ExactPenaltyMirrorDescent.jl
@@ -0,0 +1,5 @@
+module ExactPenaltyMirrorDescent
+
+greet() = print("Hello World!")
+
+end # module ExactPenaltyMirrorDescent
---
+++ src/smd.jl
@@ -0,0 +1,15 @@
+using Zygote
+
+# Implementation of SMD
+
+"""smd_update(f, μ, x, λ, v)
+
+# Arguments
+* f
+
+# Returns
+"""
+function smd_update(f, μ, x, λ, v)
+    return
+end
+
